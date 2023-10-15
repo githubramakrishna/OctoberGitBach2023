c896ed37817e8252dfc2c6eb8b06edca229b7bb5
githubramakrishna <githubramakrishna@gmail.com>
2023-10-15 10:57:51 +0530

    updated dev2 test by dev2

--- src/com/rameshsoft/gittest/Dev2Test.java
+++ src/com/rameshsoft/gittest/Dev2Test.java
@@ -3,6 +3,7 @@ package com.rameshsoft.gittest;
 public class TestOne {
 	public static void main(String[] args) {
 		System.out.println("Dev2 Test one");
+		System.out.println("Dev2 updated Dev2Test");
 	}
 
 }
